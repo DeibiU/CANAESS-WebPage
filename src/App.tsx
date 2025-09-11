6c820d05b8f374953567c4845f937916ca6d4735
DeibiU <115390334+DeibiU@users.noreply.github.com>
2025-09-11 00:56:40 -0600

    Advanced project setup & Directory arrangement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import './App.css';
+import { Header } from './pages/Header/components/Header';
+import { Footer } from './pages/Footer/components/Footer';
+import { Landing } from './pages/Landing/components/Landing';
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <div className="App">
+    <Header />
+    <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/AboutUs/components/AboutUs.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../AboutUs.css'
+
+interface AboutUsProps {
+  
+}
+
+const AboutUs = ({}: AboutUsProps) => {
+  return (
+    <div className="AboutUs">
+    </div>
+  );
+}
+
+export { AboutUs };
