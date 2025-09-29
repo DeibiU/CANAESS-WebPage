0d8221116d26e1b855c6cd550d18f18c2bfeee01
Ignacio Zelada <109553320+IgnacioZAraya@users.noreply.github.com>
2025-09-29 17:52:47 -0600

    Tailwind fix

--- src/pages/Header/components/Header.tsx
+++ src/pages/Header/components/Header.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import Logo from './assets/CanaessLogo.png';
-import '../Header.css'
+import React from "react";
+import Logo from "../../../assets/CanaessLogo.png";
+import "../Header.css";
 
-interface HeaderProps {
-  
-}
+interface HeaderProps {}
 
 const Header = ({}: HeaderProps) => {
   return (
     <div className="Header">
+      <img src={Logo} alt="Logo" />
     </div>
   );
-}
+};
 
 export { Header };
