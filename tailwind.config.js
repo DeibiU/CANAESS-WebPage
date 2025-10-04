f63423f7c260f078cc6b43ae90db8654dec2309f
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-10-03 21:55:16 -0600

    Taliwind Config Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        canaess: {
+          100: '#0d607c',
+          200: '#1a647d',
+          300: '#225d70',
+          400: '#467787',
+          500: '#fff200',
+          600: '#fffc4a',
+          700: '#fffd6e',
+          800: '#fffd8f',
+          900: '#fffebd',
+        }
+      }
+    },
   },
   plugins: [],
 };
