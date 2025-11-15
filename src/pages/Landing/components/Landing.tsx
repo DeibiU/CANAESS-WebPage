7bf7af786a9c5291132cc92cb69b11cffe13ab66
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-11-14 21:21:25 -0600

    Visual acommodations

--- src/pages/Landing/components/Landing.tsx
+++ src/pages/Landing/components/Landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Separator } from '../../Extras/components/Separator';
+import Placeholder from '../../../assets/squarePlaceholder.jpg';
 
 interface LandingProps {
   
@@ -8,31 +9,31 @@ interface LandingProps {
 const Landing = ({}: LandingProps) => {
   return (
     <div className="Landing">
-            <Separator />
-      <div id="content-box" className = "flex flex-row">
-        <text>
+      <Separator />
+      <div id="content-box" className = "flex justify-center items-center flex-row">
+        <text className="w-[75%]">
           info
         </text>
         <div id="separator" className = "w-0 h-0 border-r-[5vw] border-r-solid border-r-transparent border-t-[14vw] border-t-solid border-t-white margin-left" />
-        <img src='' alt='Image'/>
+        <img src={Placeholder} alt='Placeholder' className="w-[25%]"/>
       </div>
       <Separator />
-      <div>
-        <img src='' alt='Image'/>
-              <div id="separator" className = "w-0 h-0 border-l-[5vw] border-l-solid border-l-transparent border-t-[14vw] border-t-solid border-t-white margin-left" />
-        <text>
+      <div id="content-box" className = "flex justify-center items-center flex-row">
+        <img src={Placeholder} alt='Placeholder' className="w-[25%]"/>
+        <div id="separator" className = "w-0 h-0 border-l-[5vw] border-l-solid border-l-transparent border-t-[14vw] border-t-solid border-t-white margin-left" />
+        <text className="w-[75%]">
           info
         </text>
       </div>
-            <Separator />
-      <div>
-        <text>
+      <Separator />
+      <div id="content-box" className = "flex justify-center items-center flex-row">
+        <text className="w-[75%]">
           info
         </text>
         <div id="separator" className = "w-0 h-0 border-r-[5vw] border-r-solid border-r-transparent border-t-[14vw] border-t-solid border-t-white margin-left" />
-        <img src='' alt='Image'/>
+        <img src={Placeholder} alt='Placeholder' className="w-[25%]"/>
       </div>
-            <Separator />
+      <Separator />
     </div>
   );
 }
