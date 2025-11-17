bed173648948cc7e2eece9d42e0812298427c6f0
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-11-16 22:35:07 -0600

    Further visual accomodations

--- src/pages/AboutUs/components/ProfilePanels/Profile.tsx
+++ src/pages/AboutUs/components/ProfilePanels/Profile.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../../AboutUs.css';
+import ProfileImg from '../../../../assets/profile.jpg';
 interface ProfileProps {
   
 }
 
 const Profile = ({}: ProfileProps) => {
   return (
-    <div className="AboutUs">
-      <div>
-        <img src="" alt="Member's image" />
-        <h3>Name</h3>
-        <text name="infoMember" id="info"></text>
+    <div className="bg-canaess-100 border-[3px] min-w-[47%] py-2 px-[1%] border-[#ffb200]">
+      <div className="flex">
+        <img src={ProfileImg} alt="Member" className="w-[32%] rounded-[100%] border-[3px] border-[#ffb200]" />
+        <div>
+        <h3 className="font-semibold p-4 text-white">Name</h3>
+        <p className="text-white pt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Landing/components/Landing.tsx
+++ src/pages/Landing/components/Landing.tsx
@@ -15,11 +15,11 @@ const Landing = ({}: LandingProps) => {
           info
         </text>
         <div id="separator" className = "w-0 h-0 border-r-[5vw] border-r-solid border-r-transparent border-t-[14vw] border-t-solid border-t-white margin-left" />
-        <img src={Placeholder} alt='Placeholder' className="w-[25%]"/>
+        <img src={Placeholder} alt='Placeholder' className="w-[25%] rounded-bl-[60%] rounded-tl-[10%]"/>
       </div>
       <Separator />
       <div id="content-box" className = "flex justify-center items-center flex-row">
-        <img src={Placeholder} alt='Placeholder' className="w-[25%]"/>
+        <img src={Placeholder} alt='Placeholder' className="w-[25%] rounded-br-[60%] rounded-tr-[10%]"/>
         <div id="separator" className = "w-0 h-0 border-l-[5vw] border-l-solid border-l-transparent border-t-[14vw] border-t-solid border-t-white margin-left" />
         <text className="w-[75%]">
           info
@@ -31,7 +31,7 @@ const Landing = ({}: LandingProps) => {
           info
         </text>
         <div id="separator" className = "w-0 h-0 border-r-[5vw] border-r-solid border-r-transparent border-t-[14vw] border-t-solid border-t-white margin-left" />
-        <img src={Placeholder} alt='Placeholder' className="w-[25%]"/>
+        <img src={Placeholder} alt='Placeholder' className="w-[25%] rounded-bl-[60%] rounded-tl-[10%]"/>
       </div>
       <Separator />
     </div>
