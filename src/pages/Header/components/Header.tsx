b47047577ce05e83fff6b5c69f4b21fa39df7670
DeibiU <dhuertasl@ucenfotec.ac.cr>
2026-01-08 20:32:19 -0600

    Logo fix

--- src/pages/Header/components/Header.tsx
+++ src/pages/Header/components/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {}
 
 const Header = ({}: HeaderProps) => {
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="max-w-screen-lg mx-auto py-10">
       <Link to="/">
       <img alt='Logo' src={Logo} className='mx-auto'/></Link>
     </div>
