d0a0eed90a378d146ae94d0d95826e2a59c4e3d7
DeibiU <dhuertasl@ucenfotec.ac.cr>
2026-07-06 16:44:47 -0600

    Profile card information functions

--- src/pages/AboutUs/components/ProfilePanels/Profile.tsx
+++ src/pages/AboutUs/components/ProfilePanels/Profile.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import '../../AboutUs.css';
 import ProfileImg from '../../../../assets/profile.jpg';
+import '../../AboutUs.css';
 interface ProfileProps {
-  
+  Role: string;
+  member: string;
 }
 
-const Profile = ({}: ProfileProps) => {
+const Profile = ({ Role, member }: ProfileProps) => {
   return (
-    <div className="bg-canaess-100 border-[3px] min-w-[47%] py-2 px-[1%] border-[#ffb200]">
+    <div className="bg-canaess-100 border-[3px] py-2 max-sm:w-[100%] w-[50%] px-[1%] border-[#ffb200] rounded-[3%]">
       <div className="flex">
-        <img src={ProfileImg} alt="Member" className="w-[32%] rounded-[100%] border-[3px] border-[#ffb200]" />
-        <div>
-        <h3 className="font-semibold p-4 text-white">Name</h3>
-        <p className="text-white pt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div className="w-[32%]">
+          <img src={ProfileImg} alt="Member" className="rounded-[100%] border-[3px] border-[#ffb200]" />
+        </div>
+        <div className="w-[68%]">
+        <h2 className="font-semibold p-4 text-white">{Role}</h2>
+        <p className="text-white pt-4">{member}</p>
         </div>
       </div>
     </div>
